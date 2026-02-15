2132bf712e8d99dc799513c0f90bd2c39e860358
PierreLgol <plgol.perso@gmail.com>
2026-02-15 13:01:12 +0100

    make ft_strdup external calls pie-safe

--- src/ft_strdup.s
+++ src/ft_strdup.s
@@ -12,17 +12,17 @@ ft_strdup:
     push rbx                        ; preserve callee-saved register, align stack for calls
     mov rbx, rdi                    ; keep source pointer across calls
 
-    call ft_strlen                  ; rax = source length
+    call ft_strlen wrt ..plt        ; rax = source length
     inc rax                         ; include null terminator
 
     mov rdi, rax                    ; malloc(size)
-    call malloc
+    call malloc wrt ..plt
     test rax, rax
     je ft_strdup_malloc_failed
 
     mov rdi, rax                    ; destination
     mov rsi, rbx                    ; source
-    call ft_strcpy                  ; returns destination in rax
+    call ft_strcpy wrt ..plt        ; returns destination in rax
 
     pop rbx
     ret
